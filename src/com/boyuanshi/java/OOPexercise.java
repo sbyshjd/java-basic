2459e52aacc75f972eb95ef8399eabb0db303dd4
sbysh <sbysh@192.168.178.27>
2020-11-09 09:06:26 +0100

    The first change of the remote repository

--- src/com/boyuanshi/java/OOPexercise.java
+++ src/com/boyuanshi/java/OOPexercise.java
@@ -1,5 +1,17 @@
 package com.boyuanshi.java;
 
 public class OOPexercise {
-
+	public static void main(String[] args) {
+		OOPexercise exer1 = new OOPexercise();
+		exer1.method();
+	}
+	
+	public void method() {
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 8; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 }
